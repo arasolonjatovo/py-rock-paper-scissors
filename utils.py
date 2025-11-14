7b24be0e501989aa25583cb89b2e20e416241a13
Audrey RASOLONJATOVO <rasolonjatovo.audrey@gmail.com>
2025-11-14 17:54:55 +0100

    refactor(utils.py): standardize valid_moves declaration and add play_again function

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
 def check_user_entry(user_move):
-    valid_moves = ['rock', 'paper', 'scissors']
+    valid_moves = ["rock", "paper", "scissors"]
     return user_move in valid_moves
+
+
+def play_again():
+    user_input = input("Do you want to play again? (y/n):").lower()
+    return user_input == "y"
