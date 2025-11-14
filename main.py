34b12af3294b93abf1538d6fcf2be03a15e920b1
Audrey RASOLONJATOVO <rasolonjatovo.audrey@gmail.com>
2025-11-14 17:54:50 +0100

    feat(main.py): implement game loop and play again functionality

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from game import start_game
+from utils import play_again
 
 
 def main():
-    start_game()
+    while True:
+        start_game()
+        if not play_again():
+            print("You fought well! Bye!")
+            break
 
 
 if __name__ == "__main__":
